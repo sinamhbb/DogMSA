b753b073951fd3da8f318941329c1f332f2aed57
sinamhbb <sina.mhbbi@gmail.com>
2022-03-30 08:08:33 +0300

    Adding a single Microservice to eureka

---
+++ DogMicroservice/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Dio','Vizsla', 'Hungarian');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Kiwi','Mastiff', 'Tibetan');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Rex','Shepherd', 'German');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Devil','Poodle','Estonia');
